09d43609f196b6a9d657d547cba7f1c6d8a22cae
nitishgarg2002 <nitishgarg2002@gmail.com>
2021-01-01 21:28:50 +0530

    partitionalgos

--- Codes/Partitions/Lomuto.cpp
+++ Codes/Partitions/Lomuto.cpp
@@ -2,7 +2,7 @@ int lomutoPartition(int arr[],int l, int h){
     int pivot=arr[h];
     int i=l-1;
     for(int j=l;j<=h-1;j++){
-        if(arr[j]<pivot){
+        if(arr[j]<=pivot){
             i++;
             swap(arr[i],arr[j]);
         }
